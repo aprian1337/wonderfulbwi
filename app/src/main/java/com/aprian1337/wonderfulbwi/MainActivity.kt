b71eba47de952347d7d103ee424ca607cec66811
aprian1337 <dwikyapriyan@gmail.com>
2021-03-09 19:15:57 +0700

    init

---
+++ app/src/main/java/com/aprian1337/wonderfulbwi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aprian1337.wonderfulbwi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/aprian1337/wonderfulbwi/PreHome.kt
@@ -0,0 +1,11 @@
+package com.aprian1337.wonderfulbwi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pre_home)
+    }
+}
